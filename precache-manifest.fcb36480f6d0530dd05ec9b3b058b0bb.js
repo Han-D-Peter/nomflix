da0b635d671c4f6056c180e35b3d21157e4d0f28
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-07-24 13:43:00 +0900

    Updates

---
+++ precache-manifest.fcb36480f6d0530dd05ec9b3b058b0bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e6121260489e8b55b42",
+    "url": "/nomflix/static/js/main.3e612126.chunk.js"
+  },
+  {
+    "revision": "e7b13412d90eb5305b85",
+    "url": "/nomflix/static/js/1.e7b13412.chunk.js"
+  },
+  {
+    "revision": "621cdbff6339be2fca9f",
+    "url": "/nomflix/static/js/runtime~main.621cdbff.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "fd62639307a51cf1d1753d7ad34119cb",
+    "url": "/nomflix/index.html"
+  }
+];
